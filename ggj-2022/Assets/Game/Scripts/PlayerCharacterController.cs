d24169260458a89aeaaccbff5788f0a63c60ea3e
David Evans <david0evans@gmail.com>
2022-01-29 13:16:55 -0800

    dynamic player joining and input assignment + camera refactor

--- ggj-2022/Assets/Game/Scripts/PlayerCharacterController.cs
+++ ggj-2022/Assets/Game/Scripts/PlayerCharacterController.cs
@@ -3,19 +3,33 @@ using Rewired;
 
 public class PlayerCharacterController : MonoBehaviour
 {
-  public int RewiredPlayerId = 0;
+  public Rewired.Player RewiredPlayer;
   public GameCharacterController Character = null;
   public SlapController SlapController = null;
+  public CameraControllerStack CameraStack = null;
+  public CameraControllerPlayer PlayerCameraPrefab = null;
+
+  private CameraControllerPlayer _playerCamera;
+
+  private void Start()
+  {
+    _playerCamera = Instantiate(PlayerCameraPrefab);
+    _playerCamera.TargetPlayer = this;
+
+    CameraStack.PushController(_playerCamera);
+  }
 
   private void Update()
   {
-    var player = ReInput.players.GetPlayer(RewiredPlayerId);
-    Character.DesiredSpeed = player.GetAxis(RewiredConsts.Action.Move);
-    Character.DesiredTurn = player.GetAxis(RewiredConsts.Action.Turn);
-
-    if (player.GetButtonDown(RewiredConsts.Action.Slap))
+    if (RewiredPlayer != null)
     {
-      SlapController.Slap();
+      Character.DesiredSpeed = RewiredPlayer.GetAxis(RewiredConsts.Action.Move);
+      Character.DesiredTurn = RewiredPlayer.GetAxis(RewiredConsts.Action.Turn);
+
+      if (RewiredPlayer.GetButtonDown(RewiredConsts.Action.Slap))
+      {
+        SlapController.Slap();
+      }
     }
   }
 }
