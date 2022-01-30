807ec93370bd4eee202f4bfe3adc9361b60a9a1e
Brendan Walker <brendan@millerwalker.net>
2022-01-29 22:11:23 -0800

    Update UI progress meters when slapping

--- ggj-2022/Assets/Game/Scripts/UI/PlayerUIHandler.cs
+++ ggj-2022/Assets/Game/Scripts/UI/PlayerUIHandler.cs
@@ -6,114 +6,153 @@ using UnityEngine.UI;
 [System.Serializable]
 public struct PlayerGoalUI
 {
-    public Text bodyPartLabel;
-    public Image bodyPartIcon;
-    public RectTransform progressRectTransform;
+  public Text bodyPartLabel;
+  public Image bodyPartIcon;
+  public RectTransform progressRectTransform;
 }
 
 public class PlayerUIHandler : UIPageBase
 {
-    private ePlayer _assignedPlayer = ePlayer.Invalid;
+  private ePlayer _assignedPlayer = ePlayer.Invalid;
 
-    public List<PlayerGoalUI> RequiredGoalsUI;
-    public PlayerGoalUI BonusGoalUI;
+  public List<PlayerGoalUI> RequiredGoalsUI;
+  public PlayerGoalUI BonusGoalUI;
 
-    public Sprite BrainIcon;
-    public Sprite OpticalIcon;
-    public Sprite MouthIcon;
-    public Sprite GutsIcon;
-    public Sprite ArmsIcon;
-    public Sprite LoinsIcon;
-    public Sprite LegsIcon;
+  public Sprite BrainIcon;
+  public Sprite OpticalIcon;
+  public Sprite MouthIcon;
+  public Sprite GutsIcon;
+  public Sprite ArmsIcon;
+  public Sprite LoinsIcon;
+  public Sprite LegsIcon;
 
-    protected override void Awake()
+  protected override void Awake()
+  {
+    base.Awake();
+    Shown += OnShown;
+  }
+
+  public void AssignPlayer(ePlayer player)
+  {
+    _assignedPlayer = player;
+
+    Canvas parentCanvas = this.transform.parent.GetComponent<Canvas>();
+    parentCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+    parentCanvas.worldCamera = CameraManager.Instance.getPlayerCamera(_assignedPlayer);
+    parentCanvas.planeDistance = 1.0f;
+  }
+
+  private void OnShown()
+  {
+    if (_assignedPlayer == ePlayer.Invalid)
     {
-        base.Awake();
-        Shown += OnShown;
+      Debug.LogError("PlayerUIHandler shown without assigning a player!");
+      return;
     }
 
-    public void AssignPlayer(ePlayer player)
-    {
-        _assignedPlayer = player;
+    ScenarioManager scenarioMgr = ScenarioManager.Instance;
+    Scenario scenario = scenarioMgr.GetCurrentScenario();
+    PlayerGoals goals = scenario.GetGoalsForPlayer(_assignedPlayer);
 
-        Canvas parentCanvas = this.transform.parent.GetComponent<Canvas>();
-        parentCanvas.renderMode = RenderMode.ScreenSpaceCamera;
-        parentCanvas.worldCamera = CameraManager.Instance.getPlayerCamera(_assignedPlayer);
-        parentCanvas.planeDistance = 1.0f;
+    for (int i = 0; i < RequiredGoalsUI.Count; ++i)
+    {
+      PlayerGoalUI goalUI = RequiredGoalsUI[i];
+
+      if (i < goals.Requirements.Length)
+      {
+        eBodyPart requiredBodyPart = goals.Requirements[i];
+        SetupPlayerGoalUI(goalUI, requiredBodyPart);
+      }
+      else
+      {
+        SetupPlayerGoalUI(goalUI, eBodyPart.NONE);
+      }
     }
 
-    private void OnShown()
+    SetupPlayerGoalUI(BonusGoalUI, goals.Bonus);
+  }
+
+  void SetupPlayerGoalUI(PlayerGoalUI goalUI, eBodyPart bodyPart)
+  {
+    if (bodyPart != eBodyPart.NONE)
     {
-        if (_assignedPlayer == ePlayer.Invalid)
-        {
-            Debug.LogError("PlayerUIHandler shown without assigning a player!");
-            return;
-        }
-
-        ScenarioManager scenarioMgr = ScenarioManager.Instance;
-        Scenario scenario = scenarioMgr.GetCurrentScenario();
-        PlayerGoals goals = scenario.GetGoalsForPlayer(_assignedPlayer);
-
-        for (int i= 0; i < RequiredGoalsUI.Count; ++i)
-        {
-            PlayerGoalUI goalUI= RequiredGoalsUI[i];
-            
-            if (i < goals.Requirements.Length)
-            {
-                eBodyPart requiredBodyPart= goals.Requirements[i];
-                SetupPlayerGoalUI(goalUI, requiredBodyPart);
-            }
-            else
-            {
-                SetupPlayerGoalUI(goalUI, eBodyPart.NONE);
-            }
-        }
-
-        SetupPlayerGoalUI(BonusGoalUI, goals.Bonus);
+      goalUI.bodyPartIcon.sprite = GetSpriteForBodyPart(bodyPart);
+      goalUI.bodyPartLabel.text = Scenario.GetBodyPartUIText(bodyPart);
+    }
+    else
+    {
+      goalUI.bodyPartIcon.enabled = false;
+      goalUI.bodyPartLabel.enabled = false;
     }
 
-    void SetupPlayerGoalUI(PlayerGoalUI goalUI, eBodyPart bodyPart)
-    {
-        if (bodyPart != eBodyPart.NONE)
-        {
-            goalUI.bodyPartIcon.sprite = GetSpriteForBodyPart(bodyPart);
-            goalUI.bodyPartLabel.text = Scenario.GetBodyPartUIText(bodyPart);
-        }
-        else
-        {
-            goalUI.bodyPartIcon.enabled = false;
-            goalUI.bodyPartLabel.enabled = false;
-        }
+    goalUI.progressRectTransform.transform.localScale = new Vector3(0, 1, 1);
+  }
 
-        goalUI.progressRectTransform.transform.localScale = new Vector3(0, 1, 1);
+  void Update()
+  {
+    RefreshGoalProgressMeters();
+  }
+
+  public void RefreshGoalProgressMeters()
+  {
+    if (_assignedPlayer == ePlayer.Invalid)
+      return;
+
+    ScenarioManager scenarioMgr = ScenarioManager.Instance;
+    Scenario scenario = scenarioMgr.GetCurrentScenario();
+    PlayerGoals goals = scenario.GetGoalsForPlayer(_assignedPlayer);
+    PlayerStats playerStats = scenarioMgr.GetPlayerStats(_assignedPlayer);
+
+    for (int uiSlotIndex = 0; uiSlotIndex < RequiredGoalsUI.Count; ++uiSlotIndex)
+    {
+      PlayerGoalUI goalUI = RequiredGoalsUI[uiSlotIndex];
+
+      if (uiSlotIndex < goals.Requirements.Length)
+      {
+        eBodyPart bodyPart = goals.Requirements[uiSlotIndex];
+        float requirementProgress = playerStats.GetProgress(bodyPart);
+
+        if (goalUI.progressRectTransform != null)
+        {
+          goalUI.progressRectTransform.transform.localScale = new Vector3(requirementProgress, 1, 1);
+        }
+      }
     }
 
-    void Update()
+    // Update the bonus progress
+    if (goals.Bonus != eBodyPart.NONE)
     {
+      float bonusProgress = playerStats.GetProgress(goals.Bonus);
+
+      if (BonusGoalUI.progressRectTransform != null)
+      {
+        BonusGoalUI.progressRectTransform.transform.localScale = new Vector3(bonusProgress, 1, 1);
+      }
     }
+  }
 
-    private Sprite GetSpriteForBodyPart(eBodyPart part)
+  private Sprite GetSpriteForBodyPart(eBodyPart part)
+  {
+    switch (part)
     {
-        switch (part)
-        {
-            case eBodyPart.NONE:
-                return null;
-            case eBodyPart.Brain:
-                return BrainIcon;
-            case eBodyPart.Optical:
-                return OpticalIcon;
-            case eBodyPart.Mouth:
-                return MouthIcon;
-            case eBodyPart.Guts:
-                return GutsIcon;
-            case eBodyPart.Arms:
-                return ArmsIcon;
-            case eBodyPart.Loins:
-                return LoinsIcon;
-            case eBodyPart.Legs:
-                return LegsIcon;
-        }
-
+      case eBodyPart.NONE:
         return null;
+      case eBodyPart.Brain:
+        return BrainIcon;
+      case eBodyPart.Optical:
+        return OpticalIcon;
+      case eBodyPart.Mouth:
+        return MouthIcon;
+      case eBodyPart.Guts:
+        return GutsIcon;
+      case eBodyPart.Arms:
+        return ArmsIcon;
+      case eBodyPart.Loins:
+        return LoinsIcon;
+      case eBodyPart.Legs:
+        return LegsIcon;
     }
+
+    return null;
+  }
 }
