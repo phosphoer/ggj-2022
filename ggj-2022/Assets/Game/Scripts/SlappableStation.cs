4a1ae537c4684fc7f22b06891ce86cc65a207c05
VanillaBalk <abalk10@gmail.com>
2022-01-29 21:37:36 -0800

    meat slap sounds added to nerve ball

--- ggj-2022/Assets/Game/Scripts/SlappableStation.cs
+++ ggj-2022/Assets/Game/Scripts/SlappableStation.cs
@@ -6,9 +6,14 @@ public class SlappableStation : MonoBehaviour, ISlappable
   [SerializeField]
   private AnimationCurve _slapScaleCurve = null;
 
+  [SerializeField]
+  private SoundBank _meatSlapSounds = null;
+
   void ISlappable.ReceiveSlap(Vector3 slapOrigin, Vector3 slapDirection, float slapStrength)
   {
     StartCoroutine(SlapAnimAsync(slapDirection, slapStrength));
+
+    AudioManager.Instance?.PlaySound(_meatSlapSounds);
   }
 
   private IEnumerator SlapAnimAsync(Vector3 slapDirection, float slapStrength)
